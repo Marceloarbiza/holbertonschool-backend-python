02110d069dfba6f7a8ad9ec9b9e8b8f9b46823e4
Marcelo <3338@holbertonschool.com>
2022-04-21 12:01:43 -0300

    D

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,5 @@
+from typing import Callable
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply(num: float) -> float:
+        return num * multiplier
+    return multiply
