390e2ee5eb3332606d282cc25aecd2f7cccd1274
Marcelo <3338@holbertonschool.com>
2022-04-25 19:33:03 -0300

    F

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Multiple coroutines """
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ returns a asyncio.Task object """
+    return asyncio.create_task(wait_random(max_delay))
