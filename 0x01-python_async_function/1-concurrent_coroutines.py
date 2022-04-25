e07cd66cbcd7f66cb7df0ad0df20aebc61679b01
Marcelo <3338@holbertonschool.com>
2022-04-25 15:44:25 -0300

    D

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -18,7 +18,7 @@ async def wait_n(n: int, max_dealy: int) -> List[float]:
     that takes in 2 int arguments (in this order): n and max_delay.
     You will spawn wait_random n times with the specified max_delay.
     """
-    random_list: List[float] = []
+    random_list = []
     for x in range(n):
         random_list.append(asyncio.create_task(wait_random(max_dealy)))
 
