9f535b29f4d118a8d285208ae230a4bb0a2ddd33
Marcelo <3338@holbertonschool.com>
2022-04-25 18:10:26 -0300

    D

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,19 +7,11 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_dealy: int) -> List[float]:
-    """
-    Import wait_random from the previous python file that
-    you’ve written and write an async routine called wait_n
-    that takes in 2 int arguments (in this order): n and max_delay.
-    You will spawn wait_random n times with the specified max_delay.
-    """
-    random_list = []
-    for _ in range(n):
-        random_list.append(asyncio.create_task(wait_random(max_dealy)))
-
-    task_list_ascompleted = []
-    for t in asyncio.as_completed(random_list):
-        task_list_ascompleted.append(await t)
-
-    return task_list_ascompleted
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of completed n tasks"""
+    concurrent_tasks = [asyncio.create_task(wait_random(max_delay))
+                        for _ in range(n)]
+    completed_tasks = []
+    for task in asyncio.as_completed(concurrent_tasks):
+        completed_tasks.append(await task)
+    return completed_tasks
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -1,1 +1,29 @@
 ASYNC
+
+#!/usr/bin/env python3
+"""
+asynchronous coroutine
+"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_dealy: int) -> List[float]:
+    """
+    Import wait_random from the previous python file that
+    you’ve written and write an async routine called wait_n
+    that takes in 2 int arguments (in this order): n and max_delay.
+    You will spawn wait_random n times with the specified max_delay.
+    """
+    random_list = []
+    for _ in range(n):
+        random_list.append(asyncio.create_task(wait_random(max_dealy)))
+
+    task_list_ascompleted = []
+    for t in asyncio.as_completed(random_list):
+        task_list_ascompleted.append(await t)
+
+    return task_list_ascompleted
